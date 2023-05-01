ecf0fb7a2d0d5fd1056058da5e89333cbead3fba
Maxime <maximeviard@hotmail.fr>
2023-05-01 12:40:55 +0300

    feat: add scripts to interact with testnet

--- scripts/deployOwnerThroughFactory.ts
+++ scripts/deployOwnerThroughFactory.ts
@@ -1,19 +1,19 @@
 import { ethers } from "hardhat";
-import { Owner__factory } from "../typechain-types";
+import {bytecode as ownerBytecode} from '../artifacts/contracts/Owner.sol/Owner.json'
 
 async function main() {
-  const gasPrice = 4500000000;
-  const universalFactoryAddress = ''
+  const gasPrice = 45000000000;
+  const universalFactoryAddress = '0xe06F38A2b56B47E91C5e09b11E22a1556D392D37'
 
-  const ownerBytesCode = Owner__factory.bytecode;
-  const randomSalt = ethers.utils.randomBytes(32);
+  const randomSalt = ethers.utils.hexlify(ethers.utils.randomBytes(32));
+  console.log(`Random salt: ${randomSalt}`)
 
 
   const universalFactory = await ethers.getContractAt("LSP16UniversalFactory", universalFactoryAddress);
 
-  const contractAddress = await universalFactory.callStatic.deployCreate2(ownerBytesCode,randomSalt, {gasPrice});
+  const contractAddress = await universalFactory.callStatic.deployCreate2(ownerBytecode,randomSalt);
 
-  const tx = await universalFactory.deployCreate2(ownerBytesCode,randomSalt, {gasPrice});
+  const tx = await universalFactory.deployCreate2(ownerBytecode,randomSalt, {gasPrice});
 
   await tx.wait();
 
