921c15ff6bb4c0173d5aefb721bf3d7f504c4f70
Maxime <maximeviard@hotmail.fr>
2023-05-05 07:54:50 +0300

    feat: add mint feature

--- scripts/mintToken.ts
+++ scripts/mintToken.ts
@@ -12,7 +12,7 @@ import {ethers } from 'hardhat';
         const erc20TokenAddress = '0x7B5C5158E5AAe4244873Dd75a81E9bbBc4d0fc38';
         const erc20Contract = await ethers.getContractAt('MyToken', erc20TokenAddress);
         const gasPrice = await ethers.provider.getGasPrice();
-        await erc20Contract.mint('0x0eCC079C20DaA9fDE0e26b6d745c0b38479ff200', ethers.utils.parseEther('1000'), {gasPrice})
+        const tx = await erc20Contract.mint('0x0eCC079C20DaA9fDE0e26b6d745c0b38479ff200', ethers.utils.parseEther('1000'), {gasPrice})
 
     } catch (error) {
         console.error('Error:', error);
